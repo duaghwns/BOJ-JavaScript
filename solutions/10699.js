c9e3ed09bd77ca5edc4eefdaf1b7fb4c81d45d15
duaghwns <hj2758@gmail.com>
2022-04-27 11:24:06 +0900

    d

--- solutions/10699.js
+++ solutions/10699.js
@@ -1,4 +1,7 @@
 // 오늘 날짜 출력하기
 const curr = new Date();
+const utc = curr.getTime() + curr.getTimezoneOffset() * 60 * 1000;
+const KR_TIME_DIFF = 9 * 60* 60 * 1000;
+const kr_curr = new Date(utc + KR_TIME_DIFF);
 
-console.log(curr.substr(0,10));
+console.log(kr_curr)
