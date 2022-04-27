838ed2c740641cc1e6c4fcf323b7d7a06accdb79
duaghwns <hj2758@gmail.com>
2022-04-27 15:13:06 +0900

    오늘 날짜

--- solutions/10699.js
+++ solutions/10699.js
@@ -4,4 +4,10 @@ const utc = curr.getTime() + curr.getTimezoneOffset() * 60 * 1000;
 const KR_TIME_DIFF = 9 * 60* 60 * 1000;
 const kr_curr = new Date(utc + KR_TIME_DIFF);
 
-console.log(kr_curr)
+const y = kr_curr.getFullYear()
+let m = `${kr_curr.getMonth()+1}`
+const d = kr_curr.getDate()
+
+m = m.length<2 ? '0'+m:m;
+
+console.log(`${y}-${m}-${d}`)
